b5c0551af94d13baeb4bbd7527fcc6060fa5d882
mkidee <mkidee@Savannahs-MacBook-Air.local>
2023-03-11 16:58:59 -0700

    created pseudo code base to follow and look back to

--- script.js
+++ script.js
@@ -10,5 +10,30 @@ function writePassword() {
 
 }
 
+
+// pseudo: if the button is clicked
+      // then the user is prompted with the following questions:
+      // Which password criteria would you like to include? (length, character types - lower, upper, numeric, speical characters)
+
+      // User Prompt: How long would you like your password to be? (min: 8, max: 128)
+
+      // User prompt: Do you want your password to include lowercase letters?
+
+      // User prompt: Do you want your password to include uppercase letters?
+
+      // User prompt: Do you want your password to include numbers?
+
+      // User prompt: Do you want your password to include special characters?
+
+      // Create the password, following all criteria
+
+      // Display the password to the user once it has been created.
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword() {
+
+  //things here 
+
+});
